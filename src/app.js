f19fd1f6d4ac40458c96ccfce831898edc3f428b
Benjamin26m <benjamin26.manolios@gmail.com>
2025-11-03 12:42:54 +0100

    classe test

--- src/app.js
+++ src/app.js
@@ -16,6 +16,41 @@ class Cube {
     }
 }
 
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    isTriangle() {
+        const sides = [this.a, this.b, this.c];
+        if (sides.some(s => typeof s !== 'number' || s <= 0)) return false;
+        const [x, y, z] = sides;
+        return x + y > z && x + z > y && y + z > x;
+    }
+
+    getType() {
+        if (!this.isTriangle()) return 'not a triangle';
+        const [a, b, c] = [this.a, this.b, this.c];
+        if (a === b && b === c) return 'equilateral';
+        if (a === b || a === c || b === c) return 'isosceles';
+        return 'scalene';
+    }
+
+    getPerimeter() {
+        if (!this.isTriangle()) return null;
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        if (!this.isTriangle()) return null;
+        const s = this.getPerimeter() / 2;
+        return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+    }
+}
+
 module.exports = {
-    Cube:Cube
+    Cube: Cube,
+    Triangle: Triangle
 }
--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,5 @@
 const Cube = require('../src/app').Cube;
+const Triangle = require('../src/app').Triangle;
 const expect = require('chai').expect;
 
 describe('Testing the Cube Functions', function() {
@@ -20,4 +21,34 @@ describe('Testing the Cube Functions', function() {
         done();
     });
     
+});
+
+describe('Testing the Triangle Functions', function() {
+    it('1. Right triangle 3-4-5 (scalene)', function(done) {
+        let t1 = new Triangle(3,4,5);
+        expect(t1.isTriangle()).to.equal(true);
+        expect(t1.getType()).to.equal('scalene');
+        expect(t1.getPerimeter()).to.equal(12);
+        expect(t1.getArea()).to.equal(6);
+        done();
+    });
+
+    it('2. Equilateral triangle 2-2-2', function(done) {
+        let t2 = new Triangle(2,2,2);
+        expect(t2.isTriangle()).to.equal(true);
+        expect(t2.getType()).to.equal('equilateral');
+        expect(t2.getPerimeter()).to.equal(6);
+        expect(t2.getArea()).to.be.closeTo(Math.sqrt(3), 1e-9);
+        done();
+    });
+
+    it('3. Not a triangle 1-2-3', function(done) {
+        let t3 = new Triangle(1,2,3);
+        expect(t3.isTriangle()).to.equal(false);
+        expect(t3.getType()).to.equal('not a triangle');
+        expect(t3.getPerimeter()).to.equal(null);
+        expect(t3.getArea()).to.equal(null);
+        done();
+    });
+
 });
